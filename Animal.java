e835f9ca9c340945890057b69fa18879742ed491
Karlp-Web <karlenportillo1@gmail.com>
2025-04-07 18:18:34 +0000

    yomomma

--- Animal.java
+++ Animal.java
@@ -1,4 +1,24 @@
 public class Animal
 {
-    
+    private String name;
+    private String species;
+
+    public Animal()
+    {
+        name = "This is the default name ";
+
+        species = "This is the defualt species ";
+
+        //can also just do this 
+
+        this(null,null);
+    }
+
+    public Animal(String name, String species)
+    {
+        this.name = name;
+
+        this.species = species;
+    }
+
 }
